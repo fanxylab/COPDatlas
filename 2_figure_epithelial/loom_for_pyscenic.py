ade079031157ef6387fc973c79b65063048ba80f
Zhuo Ma <124136294+zhuoma888@users.noreply.github.com>
2025-11-07 11:50:54 +0800

    Update loom_for_pyscenic.py

--- 2_figure_epithelial/loom_for_pyscenic.py
+++ 2_figure_epithelial/loom_for_pyscenic.py
@@ -10,11 +10,11 @@ COPD.obs["celltype"] = COPD.obs["celltype"].str.replace(' ', '_')
 
 
 #load DEGs
-DEG_HCHS = pd.read_csv("/datf/mazhuo/jupyter_notebook/COPD/tables/COPD_DEG_wilcox_HCHS_filter.csv", index_col=0)
+DEG_HCHS = pd.read_csv("./tables/COPD_DEG_wilcox_HCHS_epi_filter.csv", index_col=0)
 DEG_HCHS["celltype"] = DEG_HCHS["celltype"].str.replace(' ', '_')
-DEG_HSG12 = pd.read_csv("/datf/mazhuo/jupyter_notebook/COPD/tables/COPD_DEG_wilcox_HSG12_filter.csv", index_col=0)
+DEG_HSG12 = pd.read_csv("./tables/COPD_DEG_wilcox_HSG12_epi_filter.csv", index_col=0)
 DEG_HSG12["celltype"] = DEG_HSG12["celltype"].str.replace(' ', '_')
-DEG_G1234 = pd.read_csv("/datf/mazhuo/jupyter_notebook/COPD/tables/COPD_DEG_wilcox_G1234_filter.csv", index_col=0)
+DEG_G1234 = pd.read_csv("./tables/COPD_DEG_wilcox_G1234_epi_filter.csv", index_col=0)
 DEG_G1234["celltype"] = DEG_G1234["celltype"].str.replace(' ', '_')
 
 #filter genes
@@ -22,13 +22,9 @@ HCHS = COPD[COPD.obs["Group"].isin(["HC","HS"])].copy()
 HSG12 = COPD[COPD.obs["Group"].isin(["HS","Mild COPD"])].copy()
 G1234 = COPD[COPD.obs["Group"].isin(["Mild COPD","Severe COPD"])].copy()
 
+epi_celltype = ["Immature_AT1","AT1","AT2","TRB_Secretory","PreTB_Secretory","Basal","Goblet","Ciliated","PNEC"]
 #output loom files
-for cell in ["Immature_AT1","AT1","AT2","TRB_Secretory","PreTB_Secretory","Basal","Goblet","Ciliated","PNEC",
-                 "Adventitial_fibroblast","Alveolar_fibroblast","Myofibroblast","Fibromyocyte","Pericyte","ASM",
-                 "VSM","Mesothelial","Lymphatic","Aerocyte","gCap","Arterial","Venous","B","Plasma","Treg","CD4+_T",
-                 "CD8+_T","Proliferating_T","NKT","NK","Basophil","cDC1","cDC2","Migratory_DC","pDC","Classical_monocyte","Non-classical_monocyte",
-                 "Alveolar_macrophage","Monocyte-derived_macrophage","Interstitial_macrophage","Mast","Neutrophil"]:
-    
+for cell in epi_celltype:
     gene = list(set(DEG_HCHS["gene"][DEG_HCHS.celltype == cell].tolist()))
     if len(gene) >= 10:
         cell_ann = HCHS[HCHS.obs["celltype"] == cell].copy()
@@ -45,12 +41,7 @@ for cell in ["Immature_AT1","AT1","AT2","TRB_Secretory","PreTB_Secretory","Basal
             data_matrix = cell_ann.X
         lp.create("./SCENIC/" + cell + "_DEG_HCHS.loom", data_matrix.transpose(), row_attrs, col_attrs)
 
-for cell in ["Immature_AT1","Mature_AT1","AT2","TRB_Secretory","PreTB_Secretory","Basal","Goblet","Ciliated","PNEC",
-                 "Adventitial_fibroblast","Alveolar_fibroblast","Myofibroblast","Fibromyocyte","Pericyte","ASM",
-                 "VSM","Mesothelial","Lymphatic","Aerocyte","gCap","Arterial","Venous","B","Plasma","Treg","CD4+_T",
-                 "CD8+_T","Proliferating_T","NKT","NK","Basophil","cDC1","cDC2","Migratory_DC","pDC","Classical_monocyte","Non-classical_monocyte",
-                 "Alveolar_macrophage","Monocyte-derived_macrophage","Interstitial_macrophage","Mast","Neutrophil"]:
-    
+for cell in epi_celltype:
     gene = list(set(DEG_HSG12["gene"][DEG_HSG12.celltype == cell].tolist()))
     if len(gene) >= 10:
         cell_ann = HSG12[HSG12.obs["celltype"] == cell].copy()
@@ -67,12 +58,7 @@ for cell in ["Immature_AT1","Mature_AT1","AT2","TRB_Secretory","PreTB_Secretory"
             data_matrix = cell_ann.X
         lp.create("./SCENIC/" + cell + "_DEG_HSG12.loom", data_matrix.transpose(), row_attrs, col_attrs)
 
-for cell in ["Immature_AT1","Mature_AT1","AT2","TRB_Secretory","PreTB_Secretory","Basal","Goblet","Ciliated","PNEC",
-                 "Adventitial_fibroblast","Alveolar_fibroblast","Myofibroblast","Fibromyocyte","Pericyte","ASM",
-                 "VSM","Mesothelial","Lymphatic","Aerocyte","gCap","Arterial","Venous","B","Plasma","Treg","CD4+_T",
-                 "CD8+_T","Proliferating_T","NKT","NK","Basophil","cDC1","cDC2","Migratory_DC","pDC","Classical_monocyte","Non-classical_monocyte",
-                 "Alveolar_macrophage","Monocyte-derived_macrophage","Interstitial_macrophage","Mast","Neutrophil"]:
-    
+for cell in epi_celltype:
     gene = list(set(DEG_G1234["gene"][DEG_G1234.celltype == cell].tolist()))
     if len(gene) >= 10:
         cell_ann = G1234[G1234.obs["celltype"] == cell].copy()
